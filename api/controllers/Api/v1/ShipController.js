36d6d9632bf12cf7f28b3b0df5d67a7cd500075e
Aldo Ziflaj <aldoziflaj95@gmail.com>
2015-11-03 09:54:19 +0100

    added api/v1/ship resource with a Rails-like controller

---
+++ api/controllers/Api/v1/ShipController.js
@@ -0,0 +1,78 @@
+/**
+ * Api/v1/ShipController
+ *
+ * @description :: Server-side logic for managing api/v1/ships
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+	/**
+   * `Api/v1/ShipController.index()`
+   */
+  index: function (req, res) {
+    return res.json({
+      todo: 'index() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `Api/v1/ShipController.new()`
+   */
+  new: function (req, res) {
+    return res.json({
+      todo: 'new() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `Api/v1/ShipController.create()`
+   */
+  create: function (req, res) {
+    return res.json({
+      todo: 'create() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `Api/v1/ShipController.show()`
+   */
+  show: function (req, res) {
+    return res.json({
+      todo: 'show() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `Api/v1/ShipController.edit()`
+   */
+  edit: function (req, res) {
+    return res.json({
+      todo: 'edit() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `Api/v1/ShipController.update()`
+   */
+  update: function (req, res) {
+    return res.json({
+      todo: 'update() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `Api/v1/ShipController.destroy()`
+   */
+  destroy: function (req, res) {
+    return res.json({
+      todo: 'destroy() is not implemented yet!'
+    });
+  }
+};
