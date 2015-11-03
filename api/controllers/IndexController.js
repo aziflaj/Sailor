057811c39935294fc2bccab6c665b8e989759f25
Aldo Ziflaj <aldoziflaj95@gmail.com>
2015-11-03 09:15:56 +0100

    changed favicon

---
+++ api/controllers/IndexController.js
@@ -0,0 +1,27 @@
+/**
+ * IndexController
+ *
+ * @description :: Server-side logic for managing indices
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+  /**
+   * `IndexController.index()`
+   */
+  index: function (req, res) {
+    return res.json({
+      todo: 'index() is not implemented yet!'
+    });
+  },
+
+	/**
+	 * `IndexController.create()`
+	 */
+	create: function (req, res) {
+		return res.json({
+			todo: 'create() is not implemented yet!'
+		});
+	},
+
+};
